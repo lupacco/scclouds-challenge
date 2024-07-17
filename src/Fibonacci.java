a04eb67ddc88464f43e34dec482ea29a824f60c3
lupacco <pagottolc@gmail.com>
2024-07-17 16:29:29 -0300

    fix: Making input 0 a valid input

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,13 +1,15 @@
 public class Fibonacci {
 
     public static int recursiveSolution(int n){
+        if(n < 0) return -1;
+        if(n <= 1) return n;
 
         return 0;
     }
 
     public static int linearSolution(int n){
-        if(n < 1) return -1;
-        if(n == 1) return n;
+        if(n < 0) return -1;
+        if(n <= 1) return n;
 
         int last = 0;
         int current = 1;
@@ -20,5 +22,10 @@ public class Fibonacci {
         }
 
         return solution;
+    }
+
+    public static void printResult(int n, int result){
+        if(result < 0) System.out.println("Valor inválido");
+        else System.out.println("O "+n+"º elemento da sequencia de fibonacci é "+ result);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        int n = 11;
+        int n = 3;
 
         System.out.println("Solução Linear:");
         int resultLinear = Fibonacci.linearSolution(n);
-        if(resultLinear < 0) System.out.println("Valor inválido");
-        else System.out.println("O "+n+"º elemento da sequencia de fibonacci é "+ resultLinear);
+        Fibonacci.printResult(n, resultLinear);
     }
 }
 
